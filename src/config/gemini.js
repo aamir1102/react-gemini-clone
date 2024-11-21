7956c44a0bfc85f6733e384894f509142b16585e
aamir ahmed <aamir.aquarius2000@gmail.com>
2024-11-22 01:04:32 +0530

    initial commit

---
+++ src/config/gemini.js
@@ -0,0 +1,37 @@
+// const apiKey = "AIzaSyDtMPriDxvFXEVim2ghswTKRFay4TaiJBs";
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyDtMPriDxvFXEVim2ghswTKRFay4TaiJBs";
+// const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+
+  return result.response.text();
+}
+
+export default run;
